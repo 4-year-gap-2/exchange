32ebf3c4da5485a8e42478b423f60e6798be1765
moon <ko5642027@naver.com>
2025-04-03 20:41:41 +0900

    eureka first commit

---
+++ eureka/src/test/java/com/exchange/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exchange.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
