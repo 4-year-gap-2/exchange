7c836944d965704c65d4719981d1b13c75b19efc
moon <ko5642027@naver.com>
2025-04-11 01:05:17 +0900

    [feat] V3 체결 로직 추가

---
+++ matching/src/main/java/com/exchange/matching/infrastructure/external/MatchingEventConsumer.java
@@ -0,0 +1,22 @@
+package com.exchange.matching.infrastructure.external;
+
+import com.exchange.matching.application.command.CreateMatchingCommand;
+import com.exchange.matching.domain.service.MatchingService;
+import com.exchange.matching.infrastructure.dto.KafkaMatchingEvent;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MatchingEventConsumer {
+
+    private final MatchingService matchingService;
+
+    public MatchingEventConsumer(MatchingService matchingServiceV3) {
+        this.matchingService = matchingServiceV3;
+    }
+
+    @KafkaListener(topics = "matching-events", groupId = "matching-service")
+    public void consume(KafkaMatchingEvent event) {
+        matchingService.matchOrders(event);
+    }
+}
