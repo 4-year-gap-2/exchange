502f28bee12f4993106a536caf931693a8023121
kylim99 <zxzx05050@naver.com>
2025-04-13 21:30:41 +0900

    [feat] 분산락 카프카 테스트 진행중

--- matching/src/main/java/com/exchange/matching/infrastructure/external/MatchingEventConsumer.java
+++ matching/src/main/java/com/exchange/matching/infrastructure/external/MatchingEventConsumer.java
@@ -1,6 +1,7 @@
 package com.exchange.matching.infrastructure.external;
 
 import com.exchange.matching.application.command.CreateMatchingCommand;
+import com.exchange.matching.application.service.MatchingFacade;
 import com.exchange.matching.domain.service.MatchingService;
 import com.exchange.matching.domain.service.MatchingServiceV2;
 import com.exchange.matching.infrastructure.dto.KafkaMatchingEvent;
@@ -8,17 +9,20 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
+
 public class MatchingEventConsumer {
 
     private final MatchingServiceV2 matchingService;
+    private final MatchingFacade matchingFacade;
 
-    public MatchingEventConsumer(MatchingServiceV2 matchingServiceV3) {
+    public MatchingEventConsumer(MatchingServiceV2 matchingServiceV3, MatchingFacade matchingFacade) {
         this.matchingService = matchingServiceV3;
+        this.matchingFacade = matchingFacade;
     }
 
     @KafkaListener(topics = "matching-events", groupId = "matching-service")
     public void consume(KafkaMatchingEvent event) {
         CreateMatchingCommand command = KafkaMatchingEvent.commandFromEvent(event);
-        matchingService.matchOrders(command);
+        matchingFacade.match(command);
     }
 }
