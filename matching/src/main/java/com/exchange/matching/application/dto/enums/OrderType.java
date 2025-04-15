5b4dd94e6a5eeeca86b0c4c2c74db890244bbe25
kylim99 <zxzx05050@naver.com>
2025-04-15 16:37:34 +0900

    [fix] 레디스 문자열시간 저장 수정

--- matching/src/main/java/com/exchange/matching/application/dto/enums/OrderType.java
+++ matching/src/main/java/com/exchange/matching/application/dto/enums/OrderType.java
@@ -1,6 +1,15 @@
 package com.exchange.matching.application.dto.enums;
 
+import com.exchange.matching.domain.entiry.Order;
+
 public enum OrderType {
     BUY,  // 매수
-    SELL  // 매도
+    SELL;// 매도
+    public OrderType getOPP(OrderType orderType) {
+        if (orderType == BUY) {
+            return OrderType.SELL;
+        } else {
+           return OrderType.BUY;
+        }
+    }
 }
