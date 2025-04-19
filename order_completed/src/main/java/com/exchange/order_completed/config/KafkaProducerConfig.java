cd7a92defe751378ecb3d46f0710cc18080f7b2a
leeseowoo <leeseowoo.kr@gmail.com>
2025-04-19 22:47:43 +0900

    [chore] 불필요한 import 제거

--- order_completed/src/main/java/com/exchange/order_completed/config/KafkaProducerConfig.java
+++ order_completed/src/main/java/com/exchange/order_completed/config/KafkaProducerConfig.java
@@ -2,16 +2,13 @@ package com.exchange.order_completed.config;
 
 import com.exchange.order_completed.infrastructure.dto.KafkaOrderStoreEvent;
 import org.apache.kafka.clients.CommonClientConfigs;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.config.SaslConfigs;
-import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.*;
-import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.HashMap;
