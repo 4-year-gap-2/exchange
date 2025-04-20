c71d5e16ebe3509dd028670f4d0def65673a5d94
leeseowoo <leeseowoo.kr@gmail.com>
2025-04-20 14:02:25 +0900

    [feat] 멱등성 보장을 위한 사전 검증 로직 추가

--- order_completed/src/main/java/com/exchange/order_completed/domain/repository/CompletedOrderReader.java
+++ order_completed/src/main/java/com/exchange/order_completed/domain/repository/CompletedOrderReader.java
@@ -1,4 +1,10 @@
 package com.exchange.order_completed.domain.repository;
 
+import com.exchange.order_completed.domain.entiry.CompletedOrder;
+
+import java.util.UUID;
+
 public interface CompletedOrderReader {
+
+    CompletedOrder findByUserIdAndOrderId(UUID userId, UUID orderId);
 }
