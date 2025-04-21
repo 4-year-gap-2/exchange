c36de699ead4596ef31a49cf845d4f7b5aaddeed
kylim99 <zxzx05050@naver.com>
2025-04-21 20:50:28 +0900

    [feat] slack 알림 템플릿 수정

--- notification/src/main/java/com/springcloud/management/application/dto/CreateSlackCommand.java
+++ notification/src/main/java/com/springcloud/management/application/dto/CreateSlackCommand.java
@@ -26,6 +26,25 @@ public record CreateSlackCommand(UUID orderId,
     }
 
     public String createMessage(String topic){
-        return this.toString() + topic;
+        return String.format(
+                "에러가 발생했습니다.\n" +
+                        "에러 토픽: %s\n" +
+                        "발생 이벤트 정보\n" +
+                        "  주문 ID: %s\n" +
+                        "  거래 쌍: %s\n" +
+                        "  가격: %s\n" +
+                        "  수량: %s\n" +
+                        "  구매자 ID: %s\n" +
+                        "  판매자 ID: %s\n" +
+                        "  주문 유형: %s",
+                topic,
+                this.orderId(),
+                this.tradingPair(),
+                this.price(),
+                this.quantity(),
+                this.buyer(),
+                this.seller(),
+                this.orderType()
+        );
     }
 }
