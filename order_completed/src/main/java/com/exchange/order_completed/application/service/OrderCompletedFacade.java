e5973a917b4fd3266e780c2aadfa720c555440ee
leeseowoo <leeseowoo.kr@gmail.com>
2025-04-22 13:03:13 +0900

    [feat] 카산드라 경량 트랜잭션을 이용한 멱등성 보장 기능 구현

--- order_completed/src/main/java/com/exchange/order_completed/application/service/OrderCompletedFacade.java
+++ order_completed/src/main/java/com/exchange/order_completed/application/service/OrderCompletedFacade.java
@@ -8,6 +8,9 @@ import com.exchange.order_completed.infrastructure.dto.KafkaBalanceIncreaseEvent
 import com.exchange.order_completed.infrastructure.external.KafkaEventPublisher;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.cassandra.core.CassandraTemplate;
+import org.springframework.data.cassandra.core.InsertOptions;
+import org.springframework.data.cassandra.core.WriteResult;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,9 +21,9 @@ public class OrderCompletedFacade {
     private final CompletedOrderStore completedOrderStore;
     private final CompletedOrderReader completedOrderReader;
     private final KafkaEventPublisher kafkaEventPublisher;
+    private final CassandraTemplate cassandraTemplate;
 
     public void completeOrder(CreateOrderStoreCommand command) {
-
         CompletedOrder persistedCompletedOrder = completedOrderReader.findByUserIdAndOrderId(command.userId(), command.orderId());
 
         if (persistedCompletedOrder != null) {
@@ -29,8 +32,30 @@ public class OrderCompletedFacade {
         }
 
         CompletedOrder newCompletedOrder = command.toEntity();
+
         completedOrderStore.save(newCompletedOrder);
 
         kafkaEventPublisher.publishMessage(KafkaBalanceIncreaseEvent.from(command));
     }
+
+    @Deprecated(forRemoval = true)
+    public void completeOrder2(CreateOrderStoreCommand command) {
+        CompletedOrder newCompletedOrder = command.toEntity();
+
+        // 경량 트랜잭션 LWT: IF NOT EXISTS 옵션 생성
+        InsertOptions options = InsertOptions.builder()
+                .withIfNotExists()
+                .build();
+
+        // 내부적으로 INSERT ~ IF NOT EXISTS 실행
+        WriteResult result = cassandraTemplate.insert(newCompletedOrder, options);
+
+        if (!result.wasApplied()) {
+            log.info("이미 완료된 주문입니다. orderId={}", newCompletedOrder.getOrderId());
+            return;
+        }
+
+        kafkaEventPublisher.publishMessage(KafkaBalanceIncreaseEvent.from(command));
+    }
+
 }
