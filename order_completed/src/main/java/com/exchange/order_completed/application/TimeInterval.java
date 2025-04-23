52bd8068959b894870abad6fd2ebbcb6d99b135d
kylim99 <zxzx05050@naver.com>
2025-04-24 01:50:06 +0900

    [feat] 1일 3시간 차트 기능 추가

--- order_completed/src/main/java/com/exchange/order_completed/application/TimeInterval.java
+++ order_completed/src/main/java/com/exchange/order_completed/application/TimeInterval.java
@@ -6,10 +6,12 @@ public enum TimeInterval {
     FIVE_MINUTES("m5","minute"),
     FIFTEEN_MINUTES("m15","fifteen_minute"),
     THIRTY_MINUTES("m30","thirty_minute"),
-    ONE_HOUR("h1","one_hour"),
+    ONE_HOUR("h1","hour"),
+    THREE_HOUR("h1","three_hour"),
     SIX_HOURS("h6","six_hour"),
     TWELVE_HOURS("h12","twelve_hour"),
-    ONE_WEEK("w1","one_week"),
+    ONE_DAY("d1","day"),
+    ONE_WEEK("w1","week"),
     ONE_MONTH("mon1","month"); // 예시
 
     private final String shortCode;
