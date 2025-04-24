c1c61ccd7e914e4b0b8288d26f30ee3f40d0b75a
jyooung <cherish.yooung@gmail.com>
2025-04-24 15:02:03 +0900

    [feat] 프로젝트 예외 처리 전략 반영 - CustomNotFoundException 사용

--- user/src/main/java/com/springcloud/user/application/query/UserBalanceQueryService.java
+++ user/src/main/java/com/springcloud/user/application/query/UserBalanceQueryService.java
@@ -1,6 +1,7 @@
 package com.springcloud.user.application.query;
 
 import com.springcloud.user.application.result.FindUserBalanceResult;
+import com.springcloud.user.common.exception.CustomNotFoundException;
 import com.springcloud.user.domain.entity.User;
 import com.springcloud.user.domain.entity.UserBalance;
 import com.springcloud.user.domain.repository.UserBalanceRepository;
@@ -22,14 +23,14 @@ public class UserBalanceQueryService {
 
     //자산 조회
     public Page<FindUserBalanceResult> findBalance(UUID userId, int page, int size) {
-        //User(애그리거트 루트) 먼저 조회 (존재하지 않으면 에러)
+        // 1. User(애그리거트 루트) 먼저 조회 (존재하지 않으면 에러)
         User user = userRepository.findById(userId)
-                .orElseThrow(() -> new RuntimeException("해당 유저는 존재하지 않습니다."));
+                .orElseThrow(() -> new CustomNotFoundException("해당 유저는 존재하지 않습니다."));
         // 2. User 엔티티를 기준으로 페이징 쿼리 실행
         Pageable pageable = PageRequest.of(page, size, Sort.by("availableBalance").descending());
         Page<UserBalance> balancePage = userBalanceRepository.findByUser(user, pageable);
 
-        //페이징 처리 반환(Page의 map 메서드 활용)
+        // 3. 페이징 처리 반환(Page의 map 메서드 활용)
         return balancePage.map(FindUserBalanceResult::from);
     }
 //    // 이 부분 고민 : 코인 레파지토리를 주입받는데 이 부분이 유저파사트로 가야하나?
